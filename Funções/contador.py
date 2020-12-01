140f309f5f6a61ec195fff24361d485bb71fdc59
Ckk3 <gustavo.mota@acad.idma.edu.br>
2020-12-01 16:46:50 -0300

    Update contador.py

--- Funções/contador.py
+++ Funções/contador.py
@@ -4,3 +4,27 @@
 # b) de 10 até 0, de 2 em 2
 # c) uma contagem personalizada
 
+from time import sleep
+
+def counter(start, end, step):
+    print(f'Counting from {start} to {end} in {step} in {step}')
+    
+    if end < start and step > 0:
+        step = -step
+
+    if end < 0:
+        end -= 1
+    elif end > 0:
+        end += 1
+    elif end == 0:
+        end = -1
+
+    for c in range(start, end, step):
+        print(f'{c}', end=' ')
+        sleep(0.3)
+    print('END\n')
+
+
+counter(1, 10, 1)
+counter(10, 0, 2)
+counter(12, -10, 7)
