7478db6ced0080680a98293c379670ef138209cd
Ckk3 <gustavo.mota@acad.idma.edu.br>
2020-12-01 16:08:40 -0300

    Create print especial.py

---
+++ Funções/print especial.py
@@ -0,0 +1,9 @@
+def specialPrint(txt):
+    length = len(txt) + 6
+    print(f'~' * length)
+    print(f'   {txt}')
+    print(f'~' * length)
+
+specialPrint('Luis Gustavo')
+specialPrint('Ifma')
+specialPrint('Flamengo 2 x 0 River Plate gol do Gabigol')
