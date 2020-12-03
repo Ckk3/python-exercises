4251eb7e4a6c398a1f04f46aac138ac807e9bd99
Ckk3 <gustavo.mota@acad.idma.edu.br>
2020-12-03 00:49:17 -0300

    Create explicação 2.py

---
+++ Funções/explicação 2.py
@@ -0,0 +1,15 @@
+
+def somar(a, b, c=0):
+    '''
+
+    :param a: First number
+    :param b: Second number
+    :param c: Third number (optional)
+    :return: Nothing
+    '''
+
+    res = a + b + c
+    print(res)
+
+
+somar(6, 9)
